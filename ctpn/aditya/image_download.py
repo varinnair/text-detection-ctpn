ffe555bac8d019f81d2d097b918a7b16e1b32e1d
Varin Nair <varinnair@gmail.com>
2018-06-26 15:35:35 +0530

    setting virtual env

--- ctpn/aditya/image_download.py
+++ ctpn/aditya/image_download.py
@@ -4,21 +4,8 @@
 
 from google_images_download import google_images_download   #importing the library
 response = google_images_download.googleimagesdownload()   #class instantiation
-list = ["bank cheque india","bank cheque usa","bank cheque uk","Copy of bank cheque"]
+#list = ["bank cheque india","bank cheque usa","bank cheque uk","Copy of bank cheque"]
 
-for i in list:
-    arguments = {"keywords" : i,"limit" :100,"output_directory":"/home/aditya/research/dum1","image_directory":i,"print_urls":True}   #creating list of arguments
-    paths = response.download(arguments)   #passing the arguments to the function
-    print(paths)   #printing absolute paths of the downloaded images
-
-
-
-
-
-
-
-
-
-
-
-
+arguments = {"keywords" : "random images","limit" :400,"output_directory":"C:/Users/varin/Documents/GitHub/text-detection-ctpn/ctpn/aditya","image_directory":"random_images","print_urls":True}   #creating list of arguments
+paths = response.download(arguments)   #passing the arguments to the function
+print(paths)   #printing absolute paths of the downloaded images
