9a3774e4027859c4b870801f321646407f3a6a05
Varin Nair <varinnair@gmail.com>
2018-07-02 12:14:27 +0530

    modifications to selenium web scraper

--- classifier-docs/image_download.py
+++ classifier-docs/image_download.py
@@ -8,7 +8,7 @@ from google_images_download import google_images_download   #importing the libra
 response = google_images_download.googleimagesdownload()   #class instantiation
 #list = ["bank cheque india","bank cheque usa","bank cheque uk","Copy of bank cheque"]
 
-arguments = {"keywords" : "random images","limit" :400,"output_directory":"C:/Users/varin/Documents/GitHub/text-detection-ctpn/classifier-docs/testing","image_directory":"random-images","print_urls":True}   #creating list of arguments
-arguments['chromedriver'] = "C:/Users/varin/Documents/GitHub/text-detection-ctpn/classifier-docs/chromedriver.exe"
+arguments = {"keywords" : "random","limit" :10,"output_directory":"C:/Users/varin/Documents/GitHub/text-detection-ctpn/classifier-docs/testing","image_directory":"random-images","print_urls":True}   #creating list of arguments
+arguments['chromedriver'] = "chromedriver.exe"
 paths = response.download(arguments)   #passing the arguments to the function
 print(paths)   #printing absolute paths of the downloaded images
